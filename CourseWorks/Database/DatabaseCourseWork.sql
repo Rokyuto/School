e71830de7ecf3f76a73e79a963b672950d6cc5cf
Rokyuto <viktorvalerievasenov6@gmail.com>
2023-01-28 21:38:08 +0200

    Course Works

--- CourseWorks/Database/DatabaseCourseWork.sql
+++ CourseWorks/Database/DatabaseCourseWork.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE Airline;
 USE Airline;
-CREATE TABLE Flights(FlightID INT NOT NULL,
+CREATE TABLE Flights(/*FlightID INT AUTO_INCREMENT NOT NULL,*/
 					FlightAirline VARCHAR(60) NOT NULL,
-					FlightDate VARCHAR(20) NOT NULL,
-					FlightTime VARCHAR(20) NOT NULL,
-					FlightDuration DOUBLE NOT NULL,
+					FlightDate DATE NOT NULL,
+					FlightTime TIME NOT NULL,
+					FlightDuration TIME NOT NULL,
 					FlightStartAirport VARCHAR(60) NOT NULL,
 					FlightEndAirport VARCHAR(60) NOT NULL,
-					FlightPlane VARCHAR(40) NOT NULL,
+					FlightAircraft VARCHAR(40) NOT NULL,
 					FlightPilot VARCHAR(60) NOT NULL,
-					FlightFoodType VARCHAR(60) NOT NULL,
-					PRIMARY KEY(FlightID));
+					FlightFoodType VARCHAR(60) NOT NULL);
+					/*PRIMARY KEY(FlightID));
