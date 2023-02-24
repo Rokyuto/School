5802ac9772c24829535e85571653fd5db8f66824
Rokyuto <93655294+Rokyuto@users.noreply.github.com>
2023-02-24 22:26:27 +0200

    Database Course Work in progress

--- CourseWorks/Database/DatabaseCourseWork.sql
+++ CourseWorks/Database/DatabaseCourseWork.sql
@@ -1,13 +1,82 @@
-CREATE DATABASE Airline;
-USE Airline;
-CREATE TABLE Flights(/*FlightID INT AUTO_INCREMENT NOT NULL,*/
-					FlightAirline VARCHAR(60) NOT NULL,
-					FlightDate DATE NOT NULL,
-					FlightTime TIME NOT NULL,
-					FlightDuration TIME NOT NULL,
-					FlightStartAirport VARCHAR(60) NOT NULL,
-					FlightEndAirport VARCHAR(60) NOT NULL,
-					FlightAircraft VARCHAR(40) NOT NULL,
-					FlightPilot VARCHAR(60) NOT NULL,
-					FlightFoodType VARCHAR(60) NOT NULL);
-					/*PRIMARY KEY(FlightID));
+create database Aircompany;
+use Aircompany;
+
+create table Pilot(
+	PilotID int auto_increment not null primary key,
+    PilotName varchar(60) not null
+);
+
+create table Aircraft(
+	AircraftID int auto_increment not null primary key,
+    AircraftModel varchar(60) not null
+);
+
+create table Food(
+	FoodID int auto_increment not null primary key,
+    FoodName varchar(60) not null
+);
+
+create table Flights(
+	FlightID int auto_increment not null primary key,
+	FlightDate date not null,
+	FlightDepartTime time not null,
+	FlightDuration time not null,
+	FlightStartAirport varchar(60) not null,
+	FlightEndAirport varchar(60) not null
+	/*
+    FlightAircraft int not null,
+	FlightPilot int not null,
+	FlightFoodType int not null,
+    foreign key(FlightPilot) references Pilot(PilotID),
+    foreign key(FlightAircraft) references Aircraft(AircraftID)
+    foreign key(FlightFoodType) references Food(FoodID)
+    
+    */
+);
+
+/* Pilot to Flight M:M Connection Table */
+create table PilotToFlight(
+	PilotID int not null,
+    FlightID int not null,
+    primary key(PilotID,FlightID),
+    foreign key(PilotID) references Pilot(PilotID),
+    foreign key(FlightID) references Flights(FlightID)
+);
+
+/* Aircraft to Flight M:M Connection Table */
+create table AircraftToFlight(
+	AircraftID int not null,
+    FlightID int not null,
+    primary key(AircraftID,FlightID),
+    foreign key(AircraftID) references Aircraft(AircraftID),
+    foreign key(FlightID) references Flights(FlightID)
+);
+
+/* Food to Flight M:M Connection Table */
+create table FoodToFlight(
+	FoodID int not null,
+    FlightID int not null,
+    primary key(FoodID,FlightID),
+    foreign key(FoodID) references Food(FoodID),
+    foreign key(FlightID) references Flights(FlightID)
+);
+
+
+/* Наряди на пилоти - Pilots' Shifts */
+create table PilotsShifts(
+	PilotID int not null,
+	PilotShiftStartTime time not null,
+    PilotShiftEndTime time not null,
+    primary key(PilotID)
+);
+
+
+/* Заместване на пилот - Replace Pilot */
+create table ReplacePilot(
+	FlightDate date not null,
+	PilotToReplaceID int not null,
+    ReplacePilotID int not null,
+    primary key(FlightDate, PilotToReplaceID, ReplacePilotID)
+);
+
+
