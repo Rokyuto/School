f8d879fab5361af29675704e0073bf3131d02f91
Rokyuto <93655294+Rokyuto@users.noreply.github.com>
2023-03-23 09:44:06 +0200

    Update clock.sh

--- Operating Systems/clock.sh
+++ Operating Systems/clock.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+function alarm(){
+	for i in {1..10}
+		do
+			clear
+			sleep 0.5
+			echo "Wake up!" | figlet
+			sleep 0.5
+		done
+}
+
 EYE="@@"
 while true
 do
@@ -19,14 +29,11 @@ do
 	date +%T | cowsay -e $EYE
 	echo "Press Ctrl + C to exit"
 	sleep 1
-	if [[ $(date +%T) == $1 || $(date +%T) == $2 ]]
-	then
-		for i in {1..10}
-		do
-			clear
-			sleep 0.5
-			echo "Wake up!" | figlet
-			sleep 0.5
-		done
-	fi
+	for hour in $@
+	do
+		if [[ $(date +%T) == $hour ]]
+		then
+			alarm
+		fi
+	done
 done
