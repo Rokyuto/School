f1a7bfa37585a4f237b37cdb702f888181a388d5
Rokyuto <viktor.asenov.bg@gmail.com>
2023-04-06 21:33:54 +0300

    Database Course Work Rework in Process

--- CourseWorks/Database/2. SelectWithCondition.sql
+++ CourseWorks/Database/2. SelectWithCondition.sql
@@ -1,9 +1,7 @@
 USE aircompany;
-select FlightDate,FlightDepartTime,FlightDuration,FlightStartAirport,FlightEndAirport,
-AircraftModel, pilot.PilotName, food.FoodName as FlightFood
-from flights join aircraft,aircrafttoflight,food,foodtoflight,pilot,pilottoflight
-where (aircrafttoflight.AircraftID = aircraft.AircraftID and 
-aircrafttoflight.FlightID = flights.FlightID and
-foodtoflight.FoodID = food.FoodID and foodtoflight.FlightID = flights.FlightID and
-pilottoflight.PilotID = pilot.PilotID and  pilottoflight.FlightID = flights.FlightID) and
-FlightDate > '2016-03-20';
+select *
+from flights 
+join aircraft on flights.FlightAircraft = aircraft.AircraftID
+join food on flights.FlightFood = food.FoodID 
+join pilot on pilot.PilotID = flights.PilotID
+where FlightDate > '2016-00-00';
