3e3f54f3ade89866f786d26ed7769ce84b7f992e
Rokyuto <93655294+Rokyuto@users.noreply.github.com>
2023-05-09 12:07:41 +0300

    Update Test.java

--- 4kurs/JAVA/src/OOP_exersices/Building/Test.java
+++ 4kurs/JAVA/src/OOP_exersices/Building/Test.java
@@ -1,12 +1,15 @@
 package Building;
 
 public class Test {
+    public static void main(String[] args) {
+        House h1 = new House(250,500,"baavbip",3,"gbnwp");
+        House h2 = new House(500,1000,"tyopmu",6,".ui;.u");
+        House h3 = new House(300,600,"agfvsbh",2,"mntfmc");
+        House h4 = new House();
 
-    House h1 = new House(250,500,"baavbip",3,"gbnwp");
-    House h2 = new House(500,1000,"tyopmu",6,".ui;.u");
-    House h3 = new House(300,600,"agfvsbh",2,"mntfmc");
+        House[] houses = {h1,h2,h3};
+        h4 = House.findLargestHouse(houses);
+        h4.printHouse();
+    }
 
-    House[] houses = {h1,h2,h3};
-
-    House.findLargestHouse(houses);
 }
