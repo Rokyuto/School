8b10b3fe4b55415bf0e0b883c9c0c5b5a9069168
Rokyuto <93655294+Rokyuto@users.noreply.github.com>
2023-05-10 13:28:53 +0300

    Add files via upload

---
+++ 4kurs/SQL/CreateDatabaseEx/InsertQueries_19205.sql
@@ -0,0 +1,27 @@
+INSERT INTO `v_19205`.`alpinist` (`specialty`, `first_name`, `last_name`) VALUES ('Ice climbing', 'Peter', 'Todorov');
+INSERT INTO `v_19205`.`alpinist` (`specialty`, `first_name`, `last_name`) VALUES ('Mixed climbing', 'Georgi', 'Ivanov');
+INSERT INTO `v_19205`.`alpinist` (`specialty`, `first_name`, `last_name`) VALUES ('Alpin style climbing', 'Dimitar', 'Iliev');
+INSERT INTO `v_19205`.`alpinist` (`specialty`, `first_name`, `last_name`) VALUES ('Expedicion climbing', 'Ivan', 'Simeonov');
+
+INSERT INTO `v_19205`.`country` (`Code`, `population`, `Name`, `Continent`, `SurfaceArea`) VALUES ('BUL', '7000000', 'Bulgaria', 'Europe', '50000');
+INSERT INTO `v_19205`.`country` (`Code`, `population`, `Name`, `Continent`, `SurfaceArea`) VALUES ('SRB', '5000000', 'Serbia', 'Europe', '40000');
+INSERT INTO `v_19205`.`country` (`Code`, `population`, `Name`, `Continent`, `SurfaceArea`) VALUES ('GER', '15000000', 'Germany', 'Europe', '120000');
+
+INSERT INTO `v_19205`.`mountain` (`name`) VALUES ('Rila');
+INSERT INTO `v_19205`.`mountain` (`name`) VALUES ('Pirin');
+INSERT INTO `v_19205`.`mountain` (`name`) VALUES ('Old Mountain');
+INSERT INTO `v_19205`.`mountain` (`name`) VALUES ('Vardenik');
+INSERT INTO `v_19205`.`mountain` (`name`) VALUES ('Besna Kobila');
+INSERT INTO `v_19205`.`mountain` (`name`) VALUES ('Watzmann');
+INSERT INTO `v_19205`.`mountain` (`name`) VALUES ('Dreitorspitze');
+INSERT INTO `v_19205`.`mountain` (`name`) VALUES ('Brauneck');
+
+INSERT INTO `v_19205`.`peak` (`mountain_id`, `Name`, `Elevation`) VALUES ('1', 'Musala', '2925');
+INSERT INTO `v_19205`.`peak` (`mountain_id`, `Name`, `Elevation`) VALUES ('2', 'Vihren', '2914');
+INSERT INTO `v_19205`.`peak` (`mountain_id`, `Name`, `Elevation`) VALUES ('3', 'Botev Peak', '2376');
+INSERT INTO `v_19205`.`peak` (`mountain_id`, `Name`, `Elevation`) VALUES ('4', 'Veliki Strešer', '1875 ');
+INSERT INTO `v_19205`.`peak` (`mountain_id`, `Name`, `Elevation`) VALUES ('5', 'Vranje', '1923 ');
+INSERT INTO `v_19205`.`peak` (`mountain_id`, `Name`, `Elevation`) VALUES ('6', 'Mittelspitze ', '2713');
+INSERT INTO `v_19205`.`peak` (`mountain_id`, `Name`, `Elevation`) VALUES ('7', 'Partenkirchen Dreitorspitze', '2682');
+INSERT INTO `v_19205`.`peak` (`mountain_id`, `Name`, `Elevation`) VALUES ('8', 'Lenggries', '1555');
+
