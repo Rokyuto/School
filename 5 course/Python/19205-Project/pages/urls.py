7f0f65f1bf683a0624fb1aa6217547cf3d1b7b31
Rokyuto <93655294+Rokyuto@users.noreply.github.com>
2024-02-22 13:50:11 +0200

    Delete 5 course/Python/19205-Project directory

--- 5 course/Python/19205-Project/pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import PagesListView, PagesDetailView
-
-urlpatterns = [
-    path('pages/<int:pk>/',PagesDetailView.as_view(),name = 'post_detail'),
-    path('',PagesListView.as_view(),name = 'home'),
-
-]
