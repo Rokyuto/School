a21f6baa3783e3b8cd7e9b74606fcea8dc9892bd
Rokyuto <93655294+Rokyuto@users.noreply.github.com>
2024-02-29 14:13:58 +0200

    Add files via upload

---
+++ 5 course/Python/19205-Project/pages/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Pages
+# Create your views here.
+
+class PagesListView(ListView):
+    model = Pages
+    template_name = 'home.html'
+
+class PagesDetailView(DetailView):
+    model = Pages
+    template_name = 'post_detail.html'
+
+class PagesCreateView(CreateView):
+    model = Pages
+    template_name = 'post_new.html'
+    fields = ['title','author','body']
+
+class PagesUpdateView(UpdateView):
+    model = Pages
+    template_name = 'post_edit.html'
+    fields = ['title','body']
+
+class PagesDeleteView(DeleteView):
+    model = Pages
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
